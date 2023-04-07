56a5076c9093c5769d7283e4014899aec7b6aa35
Ibrahim Mirza <117998485+ibrahim-mirzaa@users.noreply.github.com>
2023-04-07 15:38:46 +0530

    Update Practical6.dart

--- Practical6.dart
+++ Practical6.dart
@@ -1,25 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main()=> runApp(
-  MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Login Page"),
-    ),
-    body: Column(
-      children: [
-        TextFormField(decoration: InputDecoration(
-          label: Text("Enter Your Name:"),
-        ),),
-        TextFormField(decoration: InputDecoration(
-          label: Text("Enter Your Email id :"),
-        ),),
-        ElevatedButton(
-          onPressed: ()=>{}, child: Text("SUBMIT"),
-        )
-      ],
-    )
-  ),
-  )
-);
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Login Karo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )
+            ),
+          ],
+        ));
+  }
+}
